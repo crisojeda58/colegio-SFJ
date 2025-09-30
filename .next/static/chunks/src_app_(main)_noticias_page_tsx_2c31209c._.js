0684a756503c1e4e41fd7aaee72ce90c8ba501a5
Cristian Ojeda <128162308+crisojeda58@users.noreply.github.com>
2025-09-30 16:57:10 -0300

    chore: ignore .next build output

--- .next/static/chunks/src_app_(main)_noticias_page_tsx_2c31209c._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_765e1eaa._.js",
-  "static/chunks/src_3de3a6d7._.js"
-],
-    source: "dynamic"
-});
