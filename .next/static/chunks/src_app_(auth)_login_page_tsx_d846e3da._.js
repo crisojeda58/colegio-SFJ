7d9f8a5941503321ab58cf2ece9cb670b682e547
Cristian Ojeda <128162308+crisojeda58@users.noreply.github.com>
2025-09-30 16:20:49 -0300

    Add .next to gitignore

---
+++ .next/static/chunks/src_app_(auth)_login_page_tsx_d846e3da._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/d9ef2_@firebase_auth_dist_esm_1475dbb8._.js",
+  "static/chunks/node_modules_@firebase_firestore_dist_index_esm_bf7383cb.js",
+  "static/chunks/node_modules_ef620678._.js",
+  "static/chunks/src_f0579a76._.js"
+],
+    source: "dynamic"
+});
---
+++ .next/static/chunks/src_app_(main)_noticias_page_tsx_d7bf9614._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_765e1eaa._.js",
+  "static/chunks/src_3de3a6d7._.js"
+],
+    source: "dynamic"
+});
